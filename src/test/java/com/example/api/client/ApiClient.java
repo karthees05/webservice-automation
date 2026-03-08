2bcb1abd80853ffbaef4c5916c9252209e156e62
kartheeswaran <karthees1985@gmail.com>
2026-03-08 01:18:59 +0000

    debug

--- src/test/java/com/example/api/client/ApiClient.java
+++ src/test/java/com/example/api/client/ApiClient.java
@@ -1,23 +1,13 @@
 package com.example.api.client;
 
 import com.example.api.config.Config;
-import com.example.api.model.DeviceObject;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class ApiClient {
 
     public ApiClient() {
         RestAssured.baseURI = Config.BASE_URL;
-    }
-
-    public Response createObject(DeviceObject device) {
-        return createObject(device, null);
-    }
-
-    public Response createObject(DeviceObject device, String apiKey) {
-        return createObject(device, apiKey, "application/json");
     }
 
     public Response createObject(Object body, String apiKey, String contentType) {
@@ -40,10 +30,6 @@ public class ApiClient {
         return request.post(Config.OBJECTS_ENDPOINT);
     }
 
-    public Response getObject(String id) {
-        return getObject(id, null, "application/json");
-    }
-
     public Response getObject(String id, String apiKey, String contentType) {
         var request = RestAssured.given();
         
@@ -56,10 +42,6 @@ public class ApiClient {
         }
         
         return request.get(Config.OBJECTS_ENDPOINT + "/" + id);
-    }
-
-    public Response listObjects() {
-        return listObjects(null, "application/json");
     }
 
     public Response listObjects(String apiKey, String contentType) {
@@ -96,9 +78,6 @@ public class ApiClient {
         return request.get(Config.OBJECTS_ENDPOINT);
     }
 
-    public Response deleteObject(String id) {
-        return deleteObject(id, null, "application/json");
-    }
 
     public Response deleteObject(String id, String apiKey, String contentType) {
         var request = RestAssured.given();
